8a6e742a242b47f0ff06d2f87a3dc493653b056f
thehassaan12 <40003071+thehassaan12@users.noreply.github.com>
2018-09-20 13:20:57 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+print("This function tells you miles driven per gallon.")
+
+xx = float (input("Enter miles driven: "))
+
+yy = int (input("Enter gallons of gas used: "))
+
+zz = xx/yy
+
+print ("Miles per gallons are:", zz)
